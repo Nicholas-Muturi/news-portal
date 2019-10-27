7bb2408ede3dc38dd0ed7de49e778349792582bc
Nicholas M <nicholasmuturi1@gmail.com>
2019-10-27 15:33:23 +0300

    [heroku] added heroku port configuration in App class

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,20 @@
+import static spark.Spark.*;
+
 public class App {
     public static void main(String[] args) {
+        /*-----------HEROKU CONFIG------------*/
+        ProcessBuilder process = new ProcessBuilder();
+        int port;
+
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 4567;
+        }
+        port(port);
+        /*------------------------------------*/
+
+
 
     }
 }
