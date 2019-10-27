1f06a46a91566d314c26e1f9a7029102f7828999
Nicholas M <nicholasmuturi1@gmail.com>
2019-10-27 23:19:00 +0300

    [test - Model] model tests pass correctly. Basic functionality achieved i.e. initialization and get object details

--- src/main/java/models/News.java
+++ src/main/java/models/News.java
@@ -1,4 +1,69 @@
 package models;
 
+import java.util.Objects;
+
 public class News {
+    private String title;
+    private String description;
+    private String type;
+    private int id;
+
+    public News(String title, String description) {
+        this.title = title;
+        this.description = description;
+        this.type = "General";
+    }
+
+    public News(String title, String description, String type) {
+        this.title = title;
+        this.description = description;
+        this.type = type;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        News news = (News) o;
+        return Objects.equals(title, news.title) &&
+                Objects.equals(description, news.description) &&
+                Objects.equals(type, news.type);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, description, type);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
--- src/test/java/models/NewsTest.java
+++ src/test/java/models/NewsTest.java
@@ -1,7 +1,44 @@
 package models;
 
+import dao.DatabaseRule;
+import org.junit.Rule;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
+@SuppressWarnings("ConstantConditions")
 public class NewsTest {
+    @Rule
+    public DatabaseRule databaseRule = new DatabaseRule();
+
+    private News simpleNews(){
+        return new News("Simple News","Nothing to report");
+    }
+
+    private News altNews(){
+        return new News("New Album","Kanye drop new album","Entertainment");
+    }
+
+    @Test
+    public void intializeCorrectly(){
+        News news = altNews();
+        assertTrue(news instanceof News);
+    }
+
+    @Test
+    public void getFunctionsWorkCorrectly(){
+        News news = simpleNews();
+        assertEquals(news.getTitle(),simpleNews().getTitle());
+        assertEquals(news.getDescription(),simpleNews().getDescription());
+        assertEquals(news.getType(),simpleNews().getType());
+    }
+
+    @Test
+    public void getDifferentNewsType(){
+        News news = simpleNews();
+        News news2 = altNews();
+        assertEquals(news.getType(),"General");
+        assertEquals(news2.getType(),"Entertainment");
+    }
 
 }
